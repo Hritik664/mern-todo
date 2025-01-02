b873efbbe4d49208f2b654a49939d0ced0c8f984
Hritik Singh <113785316+Hritik664@users.noreply.github.com>
2025-01-02 17:09:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
 // Serve static files in production
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'../client/build')))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

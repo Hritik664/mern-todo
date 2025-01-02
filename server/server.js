93472a2e6d21b8e800f9e00c75533a47b22e633b
Hritik Singh <113785316+Hritik664@users.noreply.github.com>
2025-01-02 17:04:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,12 +22,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/public'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname,'./hritik-singh/build')))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
